223247b6314298d1d25a68629bb328e229f4733b
sean-spreatics <sean@spreatics.com>
2023-03-30 19:24:57 +0900

    study: 동적 폼 전송 (ajax, axios)

---
+++ 09_form_dynamic/app.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const app = express();
+const PORT = 8000;
+
+app.set('view engine', 'ejs');
+app.use('/views', express.static(__dirname + '/views'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Routing (주소 설정)
+app.get('/', function (req, res) {
+  res.render('dynamic');
+});
+
+// 1-1. /ajax get 요청
+app.get('/ajax', function (req, res) {
+  console.log(req.query);
+  res.send(req.query);
+});
+
+// 1-2. /ajax post 요청
+app.post('/ajax', function (req, res) {
+  console.log(req.body);
+  res.send(req.body);
+});
+
+// 2-1. /axios get 요청
+app.get('/axios', function (req, res) {
+  console.log(req.query);
+  res.send(req.query);
+});
+
+// 2-2. /axios post 요청
+app.post('/axios', function (req, res) {
+  console.log(req.body);
+  res.send(req.body);
+});
+
+app.listen(PORT, function () {
+  console.log(`http://localhost:${PORT}`);
+});
