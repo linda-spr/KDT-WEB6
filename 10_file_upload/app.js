775872e8209eff6853173b36753189812a026e3b
sean-spreatics <sean@spreatics.com>
2023-04-01 10:01:23 +0900

    study: multer 세부 설정

--- 10_file_upload/app.js
+++ 10_file_upload/app.js
@@ -5,8 +5,30 @@ const PORT = 8000;
 // multer 미들웨어 사용하기
 const multer = require('multer'); // multer 불러오기
 const path = require('path'); // path 불러오기 (내장 모듈) => 파일, 폴더 경로를 쉽게 설정
-const upload = multer({
-  dest: 'uploads/', // dest: 업로드할 파일 경로 지정
+// const upload = multer({
+//   dest: 'uploads/', // dest: 업로드할 파일 경로 지정
+// });
+// multer 세부 설정
+const uploadDetail = multer({
+  storage: multer.diskStorage({
+    destination(req, file, done) {
+      // destination: 경로 설정
+      // done: callback 함수
+      done(null, 'uploads/');
+    },
+    filename(req, file, done) {
+      // 가정) apple.png 파일을 업로드
+      const ext = path.extname(file.originalname); // file.originalname에서 "확장자" 추출 => png
+      // path.basename(file.originalname, ext) => apple (확장자 제거한 파일이름만!!)
+      // Date.now() => 현재 시간 (1680309346279)
+      // => 1970년 1월 1일 0시 0분 0초를 기준으로 현재까지 경과된 밀리초
+      done(null, path.basename(file.originalname, ext) + Date.now() + ext);
+      // [파일명 + 현재시간.확장자] 형식으로 파일 업로드
+    },
+  }),
+  limits: { fileSize: 5 * 1024 * 1024 }, // 5MB로 파일 크기 제한
+  // 5 * 2^10 = 5KB
+  // 5 * 2^10 * 2^10 = 5MB
 });
 
 app.set('view engine', 'ejs');
@@ -21,7 +43,7 @@ app.get('/', (req, res) => {
 
 // single(): 하나의 파일 업로드할 때
 // single()의 매개변수: input 의 name과 일치시키기!
-app.post('/upload', upload.single('userfile'), (req, res) => {
+app.post('/upload', uploadDetail.single('userfile'), (req, res) => {
   console.log(req.file); // 업로드한 파일 정보
   // {
   //   fieldname: 'userfile', // 폼에 정의한 name
