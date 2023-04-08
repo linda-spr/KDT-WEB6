da1fabc935d6e9179323a1a39a01fdd5e297ed57
sean-spreatics <sean@spreatics.com>
2023-04-08 16:13:18 +0900

    study: mysql to sequelize

--- 14_sequelize/controller/Cvisitor.js
+++ 14_sequelize/controller/Cvisitor.js
@@ -1,4 +1,9 @@
-const Visitor = require('../model/Visitor');
+// [before]
+// const Visitor = require('../model/Visitor');
+// [after]
+// models 변수 값 = models/index.js에서 export 한 db 객체
+const models = require('../models'); // models/index.js 내보내는 값
+// console.log(models);
 
 // (1) GET / => localhost:PORT/
 exports.main = (req, res) => {
@@ -6,52 +11,138 @@ exports.main = (req, res) => {
 };
 
 // (2) GET /visitor => localhost:PORT/visitor
-exports.getVisitors = (req, res) => {
-  // [before]
-  // console.log(Visitor.getVisitors());
-  // res.render('visitor', { data: Visitor.getVisitors() });
+exports.getVisitors = async (req, res) => {
+  // [bofore] mysql db 연결!
+  // Visitor.getVisitors((result) => {
+  //   console.log('Cvisitor.js >>', result);
+  //   // => [ {}, {}, {} ]
+  //   res.render('visitor', { data: result });
+  // });
+  // => SELECT * FROM visitor;
 
-  // [after] mysql db 연결!
-  Visitor.getVisitors((result) => {
-    console.log('Cvisitor.js >>', result);
-    // => [ {}, {}, {} ]
-    res.render('visitor', { data: result });
-  });
+  // [after] sequelize
+  // ver1. then() 메서드 사용
+  // models.Visitor.findAll().then((result) => {
+  //   console.log('findAll >> ', result); // [ {}, {}, {}, {} ]
+  //   res.render('visitor', { data: result });
+  // });
+
+  // ver2. async/await 사용
+  const result = await models.Visitor.findAll();
+  console.log('findAll >> ', result); // [ {}, {}, {}, {} ]
+  res.render('visitor', { data: result });
 };
 
 // (3) POST /visitor/write
-exports.postVisitor = (req, res) => {
-  console.log(req.body);
+exports.postVisitor = async (req, res) => {
+  // [before]
+  // console.log(req.body);
+  // Visitor.postVisitor(req.body, (result) => {
+  //   console.log('Cvisitor.js >>', result); // model 코드에서 데이터를 추가한 결과인 rows.insertId
+  //   res.send({ id: result, name: req.body.name, comment: req.body.comment });
+  // });
+  // => insert into visitor(name, comment) values('${data.name}', '${data.comment}');
 
-  Visitor.postVisitor(req.body, (result) => {
-    console.log('Cvisitor.js >>', result); // model 코드에서 데이터를 추가한 결과인 rows.insertId
-    res.send({ id: result, name: req.body.name, comment: req.body.comment });
+  // [after]
+  const result = await models.Visitor.create({
+    name: req.body.name,
+    comment: req.body.comment,
   });
+  console.log('create >> ', result);
+  // visitor {
+  //   dataValues: { id: 8, name: 'sean', comment: '안뇽' },
+  //   _previousDataValues: { name: 'sean', comment: '안뇽', id: 8 },
+  //   uniqno: 1,
+  //   _changed: Set(0) {},
+  //   _options: {
+  //     isNewRecord: true,
+  //     _schema: null,
+  //     _schemaDelimiter: '',
+  //     attributes: undefined,
+  //     include: undefined,
+  //     raw: undefined,
+  //     silent: undefined
+  //   },
+  //   isNewRecord: false
+  // }
+  res.send(result);
 };
 
 // (5) DELETE /visitor/delete
-exports.deleteVisitor = (req, res) => {
-  console.log(req.body); // { id: n }
+exports.deleteVisitor = async (req, res) => {
+  // [before]
+  // console.log(req.body); // { id: n }
+  // Visitor.deleteVisitor(req.body.id, (result) => {
+  //   console.log('Cvisitor.js >> ', result);
+  //   res.send('삭제 성공!!');
+  // });
+  // => delete from visitor where id=${id}
 
-  Visitor.deleteVisitor(req.body.id, (result) => {
-    console.log('Cvisitor.js >> ', result);
-    res.send('삭제 성공!!');
+  // [after]
+  // models.Visitor.destroy({
+  //   where: { id: req.body.id },
+  // }).then(() => {
+  //   res.end();
+  // });
+
+  await models.Visitor.destroy({
+    where: { id: req.body.id },
   });
+  res.end();
 };
 
-exports.getVisitor = (req, res) => {
-  console.log('*', req.query); // { id: n }
+exports.getVisitor = async (req, res) => {
+  // [before]
+  // console.log('*', req.query); // { id: n }
+  // Visitor.getVisitor(req.query.id, (result) => {
+  //   console.log('**', result); // model callback에서 넘겨주는 rows[0] => {}
+  //   res.send(result);
+  // });
+  // => select * from visitor where id=${id};
 
-  Visitor.getVisitor(req.query.id, (result) => {
-    console.log('**', result); // model callback에서 넘겨주는 rows[0] => {}
-    res.send(result);
+  // [after]
+  const result = await models.Visitor.findOne({
+    where: { id: req.query.id },
   });
+  console.log('findOne >> ', result);
+  // visitor {
+  //   dataValues: { id: 3, name: '성춘향111', comment: '안뇽111' },
+  //   _previousDataValues: { id: 3, name: '성춘향111', comment: '안뇽111' },
+  //   uniqno: 1,
+  //   _changed: Set(0) {},
+  //   _options: {
+  //     isNewRecord: false,
+  //     _schema: null,
+  //     _schemaDelimiter: '',
+  //     raw: true,
+  //     attributes: [ 'id', 'name', 'comment' ]
+  //   },
+  //   isNewRecord: false
+  // }
+  res.send(result);
 };
 
-exports.patchVisitor = (req, res) => {
-  console.log(req.body);
+exports.patchVisitor = async (req, res) => {
+  // [before]
+  // console.log(req.body);
+  // Visitor.patchVisitor(req.body, () => {
+  //   res.send('수정 성공!');
+  // });
+  // => update visitor set name='${data.name}', comment='${data.comment}' WHERE id=${data.id}
 
-  Visitor.patchVisitor(req.body, () => {
-    res.send('수정 성공!');
-  });
+  // [after]
+  const result = await models.Visitor.update(
+    {
+      name: req.body.name,
+      comment: req.body.comment,
+    },
+    {
+      where: {
+        id: req.body.id,
+      },
+    }
+  );
+  console.log('update >> ', result);
+  res.end();
+  // res.send('수정 완료');
 };
---
+++ 14_sequelize/models/index.js
@@ -0,0 +1,42 @@
+// models/index.js
+// sequelize-cli 자동 생성한 코드
+
+// sequelize 모듈 불러오기
+const Sequelize = require('sequelize');
+
+// config.json 파일에서 개발환경일 때의 db 설정 옵션을 가져옴
+const config = require(__dirname + '/../config/config.json')['development'];
+// const a = require(__dirname + '/../config/config.json');
+// a에는 다음 정보가 찍힘
+// {
+//   "development": {
+//     "username": "user",
+//     "password": "1234",
+//     "database": "codingon",
+//     "host": "127.0.0.1",
+//     "dialect": "mysql"
+//   },
+//   "test": {},
+//   "production": {}
+// }
+// const config = a["development"]
+
+// db라는 이름의 빈 객체 정의 (다른 파일에서 사용할 수 있도록 내보내려는 값)
+const db = {};
+
+// sequelize 객체 선언시 매겨변수로 db명, 유저명, 비번, 정보전체를 넘겨줌
+const sequelize = new Sequelize(
+  config.database,
+  config.username,
+  config.password,
+  config
+);
+
+// db = { sequelize: sequelize, Sequelize: Sequelize }
+db.sequelize = sequelize;
+db.Sequelize = Sequelize;
+
+// db = { sequelize: sequelize, Sequelize: Sequelize, Visitor: "xx" }
+db.Visitor = require('./Visitor')(sequelize, Sequelize);
+
+module.exports = db;
