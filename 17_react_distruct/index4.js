e40a0794a89b22ad0373c9126d9814a51ee9b074
sean-spreatics <sean@spreatics.com>
2023-04-25 16:09:40 +0900

    study: js reduce 메서드

--- 17_react_distruct/index4.js
+++ 17_react_distruct/index4.js
@@ -79,3 +79,38 @@ console.log(arr3);
 //   { name: 'e', id: 5 },
 //   { name: 'f', id: 6 }
 // ]
+
+/////////////////////////////////////////
+// reduce(callback[, initValue])
+// 배열의 각 요소에 주어진 리듀서 함수를 실행하고, 하나의 결과값을 반환
+const numbers = [1, 2, 3, 4, 5];
+
+// 1 + 2 + 3 + 4 + 5
+const sum = numbers.reduce((acc, currentValue) => {
+  return acc + currentValue;
+});
+console.log(sum); // 15
+
+// acc (누적합)
+const sum2 = numbers.reduce((acc, currentValue, idx) => {
+  console.log(`======== ${idx} 번째 index ========`);
+  console.log(`acc : ${acc}`);
+  console.log(`currentValue : ${currentValue}`);
+
+  return acc + currentValue;
+});
+console.log(sum2); // 15
+
+// ======== 1 번째 index ========
+// acc : 1
+// currentValue : 2
+// ======== 2 번째 index ========
+// acc : 3
+// currentValue : 3
+// ======== 3 번째 index ========
+// acc : 6
+// currentValue : 4
+// ======== 4 번째 index ========
+// acc : 10
+// currentValue : 5
+// 15
